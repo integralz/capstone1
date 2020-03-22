898e2ac3deda526abdb6042293c0d8f86e72c2b3
integralz <57148432+integralz@users.noreply.github.com>
2020-03-22 20:58:31 +0900

    Update point_with_thingspeak.py

--- point_with_thingspeak.py
+++ point_with_thingspeak.py
@@ -3,7 +3,7 @@ import random
 import time
 
 for i in range(10):
-    value = random.randint(0,99)
+    value = random.randint(0,100)
     requests.get("https://api.thingspeak.com/update?api_key=5YK9E6T8P6FKETZS&field2="+str(value))
     time.sleep(20)
 
